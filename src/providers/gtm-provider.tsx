a074e20fa66301bc77d5e8938952320594881b5f
Milind Mishra <milind.mishra4@gmail.com>
2024-12-14 12:42:32 +0530

    refactor: update GTMProvider to use GoogleTagManager component for improved integration
    - Replaced Next.js Script components with GoogleTagManager for streamlined GTM integration.
    - Simplified the GTMProvider implementation by utilizing environment variable for GTM ID.
    - Enhanced loading strategy for better performance and maintainability.

--- src/providers/gtm-provider.tsx
+++ src/providers/gtm-provider.tsx
@@ -1,6 +1,4 @@
-"use client";
-
-import Script from "next/script";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 export default function GTMProvider({
   children,
@@ -9,22 +7,7 @@ export default function GTMProvider({
 }) {
   return (
     <>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GTM_ID}');
-          `,
-        }}
-      />
+      <GoogleTagManager gtmId={process.env.GTM_ID || ""} />
       {children}
     </>
   );
