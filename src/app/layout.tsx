fb7ae27733a2a2c9599faa31ba790d49cbdb5ea3
Milind Mishra <milind.mishra4@gmail.com>
2024-12-16 15:04:56 +0530

    feat: update font usage in layout and fonts module
    - Replaced the Inter font with Space Grotesk in the RootLayout component for a fresh design update.
    - Updated the fonts module to import and export Space Grotesk, ensuring consistent typography across the application.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import Header from "@/components/header";
-import { inter } from "@/lib/fonts";
+import { spaceGrotesk } from "@/lib/fonts";
 import { metadata } from "@/lib/metadata";
 import Providers from "@/providers";
 import "./globals.css";
@@ -13,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.className}`}>
+      <body className={`${spaceGrotesk.className}`}>
         <Providers>
           <div className="min-h-screen bg-background">
             <div className="w-full ~min-[20rem]/2xl:~max-w-[20rem]/[96rem] mx-auto px-4 py-4">
--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,8 +1,9 @@
-import { Inter, Instrument_Serif } from "next/font/google";
+import { Space_Grotesk, Instrument_Serif } from "next/font/google";
 
-export const inter = Inter({ subsets: ["latin"] });
+export const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+});
 export const instrumentSerif = Instrument_Serif({
   weight: ["400"],
   subsets: ["latin"],
-  variable: "--font-instrument",
 });
